85fb2bdf81c4ad580e510e094f839fda3fe21106
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-12-10 21:05:43 +0700

    update: computed properties

--- main.js
+++ main.js
@@ -2,24 +2,34 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
+      cart: 0,
+      brand: 'Vue Mastery',
       description: 'This is a great Socks',
-      image: './assets/images/socks_green.jpg',
-      inventory: 100,
-      inStock: false,
+      selectedVariant: 0,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue' , image: './assets/images/socks_blue.jpg'},
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue' , image: './assets/images/socks_blue.jpg', quantity: 0},
       ],
-      cart: 0
     }
   },
   methods: {
     addToCart() {
       this.cart += 1
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
     }
   }
 })
