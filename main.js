8bc4dbc75072e203ab3483f683a31975b8872bb9
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-12-10 15:57:05 +0700

    update: complete binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'This is a great Socks'
+      description: 'This is a great Socks',
+      image: './assets/images/socks_green.jpg'
     }
   }
 })
